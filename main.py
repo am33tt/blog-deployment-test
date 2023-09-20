a5285beeff486617b007a93f6815833b619cc6d7
Amit Yadav <amyth0530@gmail.com>
2023-09-20 15:41:58 +0545

    Another change in main.py

--- main.py
+++ main.py
@@ -16,7 +16,10 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap5(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', "sqlite:///posts.db")
+uri = os.environ.get('DATABASE_URL')
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('BOOL')
 db = SQLAlchemy()
 db.init_app(app)
