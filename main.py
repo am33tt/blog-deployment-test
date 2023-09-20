571fa9011e0cd3e8c6cb593e10bbba7d6d063a1d
Amit Yadav <amyth0530@gmail.com>
2023-09-20 15:58:23 +0545

    reverted

--- main.py
+++ main.py
@@ -16,10 +16,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap5(app)
 
 # CONNECT TO DB
-uri = os.environ.get('DATABASE_URL')
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', "sqlite:///posts.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('BOOL')
 db = SQLAlchemy()
 db.init_app(app)
